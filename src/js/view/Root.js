14e6bc488d6a9560282b977c986e055b4677b4df
chuross <azarasi.mo.mo@gmail.com>
2016-07-28 03:33:45 +0900

    ディレクトリ名を変更した

--- src/js/view/Root.js
+++ src/js/view/Root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Router, Route, browserHistory } from 'react-router'
-import Top from 'view/page/Top'
+import Top from 'view/route/Top'
 
 export default class Root extends Component {
     render() {
