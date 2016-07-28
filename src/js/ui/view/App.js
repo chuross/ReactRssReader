ea0b5dbb65da385ccd3175011af6b57d7e9968ec
chuross <azarasi.mo.mo@gmail.com>
2016-07-28 23:53:53 +0900

    ルート要素の定義が冗長化していたので、Indexを削除した

--- src/js/ui/view/App.js
+++ src/js/ui/view/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Router, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from 'ui/reducer/app'
-import Index from 'ui/view/Index'
+import routes from 'ui/view/Routes'
 
 const store = createStore(reducer);
 
 ReactDom.render(
   <Provider store={store}>
-    <Index />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
 , document.getElementById('app'));
--- src/js/ui/view/Index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { Router, browserHistory } from 'react-router'
-import routes from 'ui/view/Routes'
-
-export default class Index extends Component {
-    render() {
-      return (<Router history={browserHistory} routes={routes} />);
-    }
-}
