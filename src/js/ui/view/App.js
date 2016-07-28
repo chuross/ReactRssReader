38cc8d881959ef3f5e356122bb6a01d8feb9973d
chuross <azarasi.mo.mo@gmail.com>
2016-07-28 23:51:23 +0900

    Storeを生成するようにした

--- src/js/ui/view/App.js
+++ src/js/ui/view/App.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from 'ui/reducer/app'
 import Index from 'ui/view/Index'
 
-ReactDom.render(<Index />, document.getElementById('app'));
+const store = createStore(reducer);
+
+ReactDom.render(
+  <Provider store={store}>
+    <Index />
+  </Provider>
+, document.getElementById('app'));
